5fe4fb4115772b3929d5654911cd4455718dfc2c
soyyogi <raoyogesh78@yahoo.in>
2021-01-31 12:01:00 +0100

    added logical operators

--- operators.php
+++ operators.php
@@ -54,3 +54,82 @@ var_dump($x <= $y); // returns true because $x is less than or equal to $y
 // $x = 50;
 // var_dump($x >= $y); // returns true because $x is greater than or equal to $y
 // var_dump($x <= $y); // returns true because $x is less than or equal to $y
+
+echo '<h2>Logical Operators</h2>';
+echo '
+<pre>
+$x = 100;  
+$y = 50;
+</pre>
+<p>True if both are true</p>
+<pre>
+if ($x == 100 and $y == 50) {
+    echo "Hello world!";
+}
+</pre>';
+$x = 100;  
+$y = 50;
+
+if ($x == 100 and $y == 50) {
+    echo "Hello world!";
+}
+
+echo '
+<br><br>
+<p>True if both are true</p>
+<pre>
+if ($x == 100 && $y == 50) {
+    echo "Hello world!";
+}
+</pre>';
+if ($x == 100 && $y == 50) {
+    echo "Hello world!";
+}
+
+echo '
+<br><br>
+<p>True if either is true</p>
+<pre>
+if ($x == 100 or $y == 80) {
+    echo "Hello world!";
+}
+</pre>';
+if ($x == 100 or $y == 80) {
+    echo "Hello world!";
+}
+
+echo '
+<br><br>
+<p>True if either is true</p>
+<pre>
+if ($x == 100 || $y == 80) {
+    echo "Hello world!";
+}
+</pre>';
+if ($x == 100 || $y == 80) {
+    echo "Hello world!";
+}
+
+echo '
+<br><br>
+<p>True if it is not true</p>
+<pre>
+if ($x !== 90) {
+    echo "Hello world!";
+}
+</pre>';
+if ($x !== 90) {
+    echo "Hello world!";
+}
+
+echo '
+<br><br>
+<p>True if either is true, but both not</p>
+<pre>
+if ($x == 100 xor $y == 80) {
+    echo "Hello world!";
+}
+</pre>';
+if ($x == 100 xor $y == 80) {
+    echo "Hello world!";
+}
