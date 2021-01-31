cf542f49332da9a35c579286c18f8a3f6cf14a2e
soyyogi <raoyogesh78@yahoo.in>
2021-01-31 11:18:32 +0100

    added while and do..while loop

--- iterators.php
+++ iterators.php
@@ -9,12 +9,12 @@ for (<i>init counter; test counter; increment counter) {
 </pre>
 <h5>Example</h5>
 <pre>
-for ($x = 0; $x <= 10; $x++) {
+for ($x = 0; $x <= 5; $x++) {
     echo "The number is: $x &lt;br&gt;";
 }
 </pre>
 ';
-for ($x = 0; $x <= 10; $x++) {
+for ($x = 0; $x <= 5; $x++) {
     echo "The number is: $x <br>";
 }
 
@@ -55,3 +55,51 @@ $age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
 foreach ($age as $x => $val) {
     echo "$x = $val<br>";
 }
+
+echo '
+<h4>The while loop executes a block of code as long as the specified condition is true.</h4>
+<pre>
+while (<i>condition is true) {
+    code to be executed</i>;
+}
+</pre>
+<h5>Example</h5>
+<pre>
+$x = 1;
+
+while($x <= 5) {
+    echo "The number is: $x &lt;br&gt;";
+    $x++;
+}
+</pre>
+';
+$x = 1;
+
+while ($x <= 5) {
+    echo "The number is: $x <br>";
+    $x++;
+}
+
+echo '
+<h4>The do...while loop will always execute the block of code once, it will then check the condition, and repeat the loop while the specified condition is true.</h4>
+<pre>
+do {
+    <i>code to be executed</i>;
+  } while (<i>condition is true</i>);
+</pre>
+<h5>Example</h5>
+<pre>
+$x = 1;
+
+do {
+    echo "The number is: $x &lt;br&gt;";
+    $x++;
+} while ($x <= 5);
+</pre>
+';
+$x = 1;
+
+do {
+    echo "The number is: $x <br>";
+    $x++;
+} while ($x <= 5);
