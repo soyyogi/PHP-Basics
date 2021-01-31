cd026c043f3bed23e2db20dc30f0d7ea4a63e25a
soyyogi <raoyogesh78@yahoo.in>
2021-01-31 11:39:38 +0100

    created operators with arithmetic operators

---
+++ operators.php
@@ -0,0 +1,22 @@
+<?php
+echo '<h1>Operators</h1>
+<h2>Arithmetic Operators</h2>';
+echo '
+<pre>
+$x = 10;  
+$y = 6;
+
+echo $x + $y . \'&lt;br&gt;\';
+echo $x - $y . \'&lt;br&gt;\';
+echo $x * $y . \'&lt;br&gt;\';
+echo $x / $y . \'&lt;br&gt;\';
+echo $x % $y;
+</pre>';
+$x = 10;  
+$y = 6;
+
+echo $x + $y . '<br>';
+echo $x - $y . '<br>';
+echo $x * $y . '<br>';
+echo $x / $y . '<br>';
+echo $x % $y;
