840cd975f5ce446ce136c4268d3df16a4be76c0b
soyyogi <raoyogesh78@yahoo.in>
2021-01-31 11:49:32 +0100

    added comparison operators

--- operators.php
+++ operators.php
@@ -20,3 +20,37 @@ echo $x - $y . '<br>';
 echo $x * $y . '<br>';
 echo $x / $y . '<br>';
 echo $x % $y;
+
+echo '<h2>Comparison Operators</h2>';
+echo '
+<pre>
+$x = 100;  
+$y = "100";
+
+var_dump($x == $y); // returns true because values are equal
+var_dump($x === $y); // returns false because types are not equal
+var_dump($x != $y); // returns false because values are equal
+
+$y = 50;
+var_dump($x > $y); // returns true because $x is greater than
+
+$x = 10;
+var_dump($x < $y); // returns true because $x is less than $y
+
+$x = 50;
+var_dump($x >= $y); // returns true because $x is greater than or equal to $y
+var_dump($x <= $y); // returns true because $x is less than or equal to $y
+</pre>';
+// $x = 100;  
+// $y = "100";
+
+// var_dump($x == $y); // returns true because values are equal
+// var_dump($x === $y); // returns false because types are not equal
+// var_dump($x != $y); // returns false because values are equal
+// $y = 50;
+// var_dump($x > $y); // returns true because $x is greater than
+// $x = 10;
+// var_dump($x < $y); // returns true because $x is less than $y
+// $x = 50;
+// var_dump($x >= $y); // returns true because $x is greater than or equal to $y
+// var_dump($x <= $y); // returns true because $x is less than or equal to $y
