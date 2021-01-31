fd67dda73aa3130823a1f9bfb066d4a89f3911f9
soyyogi <raoyogesh78@yahoo.in>
2021-01-31 14:23:21 +0100

    created maths file

---
+++ maths.php
@@ -0,0 +1,59 @@
+<?php
+echo '<h1>Maths</h1>';
+echo '
+<p>Define a variable whose value is the result of the function that returns an absolute value.</p>
+<pre>
+$var = abs(-6.7);
+echo $var;
+</pre>
+';
+$var = abs(-6.7);
+echo $var;
+
+
+echo '
+<p>Define a variable whose value is the result of the function that returns a rounded value to the next highest integer.</p>
+<pre>
+$var = ceil(6.2);
+echo $var;
+</pre>
+';
+$var = ceil(6.2);
+echo $var;
+
+
+
+echo '
+<p>Define a variable whose value is the result of the function that returns the highest value of a series of values ​​that are received by parameter.</p>
+<pre>
+$var = max(0, 150, 30, 20, -8, -200);
+echo $var;
+</pre>
+';
+$var = max(0, 150, 30, 20, -8, -200);
+echo $var;
+
+
+
+
+echo '
+<p>Define a variable whose value is the result of the function that returns the lowest value of a series of values ​​that are received by parameter.</p>
+<pre>
+$var = min(0, 150, 30, 20, -8, -200);
+echo $var;
+</pre>
+';
+$var = min(0, 150, 30, 20, -8, -200);
+echo $var;
+
+
+
+echo '
+<p>Define a variable whose value is the result of the function that returns a random number</p>
+<pre>
+$var = rand(1, 10);
+echo $var;
+</pre>
+';
+$var = rand(1, 10);
+echo $var;
